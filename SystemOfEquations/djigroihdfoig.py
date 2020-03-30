6a65e215fe359f6abe9687f61e58d971ab81d86d
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2020-03-30 12:03:10 -0600

    f
    f

--- SystemOfEquations/djigroihdfoig.py
+++ SystemOfEquations/djigroihdfoig.py
@@ -10,21 +10,24 @@ def gaussSeidel(a, imax, es, l):
     n = len(a[0])
     print("\nn: " + str(n))
     x = [0.0 for i in range(n)]
-    for i in range(n):
+    for i in range(1, n):
         dummy = a[i][i]
-        for j in range(n):
-
+        for j in range(1, n):
             a[i][j] /= dummy
 
         a[i][n-1] /= dummy
-    for i in range(n):
+    for i in range(1, n):
         sum = a[i][n-1]
-        for j in range(n):
+        for j in range(1, n):
             if i != j:
                 sum -= a[i][j] * x[j]
+
         x[i] = sum
+
     iter = 1
     sentinel = 0
+
+    print(a)
     while not(sentinel == 1 or iter >= imax):
         sentinel = 1
         for i in range(n):
@@ -32,8 +35,11 @@ def gaussSeidel(a, imax, es, l):
             sum = a[i][n-1]
             for j in range(n):
                 if i != j:
+
                     sum -= a[i][j] * x[j]
+
             x[i] = l * sum + (1 - l) * old
+
             if sentinel == 1 and x[i] != 0:
                 ea = abs((x[i] - old) / x[i]) * 100
                 if ea > es:
