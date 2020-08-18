4962e603df596703485361c022eb83e79ffa6da6
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2020-08-18 08:54:40 -0600

    f
    f

---
+++ calcu.py
@@ -0,0 +1,8 @@
+
+
+
+QuadraticEquation= 28895115270389611/118755 - 28940394194731 * (2020) / 118755 + 7246969748 * (2020)**2 / 118755
+print("\n Estimate in 2020: " + str(QuadraticEquation))
+
+QuadraticEquation= 28895115270389611/118755 - 28940394194731 * (2021) / 118755 + 7246969748 * (2021)**2 / 118755
+print("\n Estimate in 2021: " + str(QuadraticEquation))
